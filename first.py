0a51c660b6a24ccb50d0097205b93812288c9d0a
Yash Tambe <109242952+YashT096@users.noreply.github.com>
2024-08-09 16:47:21 +0530

    First Day

---
+++ first.py
@@ -0,0 +1,21 @@
+#To start with ,We'll do the basic comment use in Programming World
+print("Hello World")
+
+#Basic Arithmatic operations
+print( 5 + 2 ) #Additiion
+print( 5 - 2 ) #Subtraction
+print( 5 * 2 ) #Multiplication
+print( 10 / 2 ) #Division
+print( 10 % 2 ) #modulus
+print( 3 // 2 ) #Floor division operator 
+
+#Data Types in Python used.
+
+print(type(10)) #Integer
+print(type(9.8)) #Float
+print(type(1 +6i)) #Complex
+print(type("Yash Tambe")) #String
+print(type([1,2,3,4,5])) #List
+print(type({"Name" : "Yash" , "Insta " : "theyashtambe"})) #Dictionary
+print(type({9.8 , 4.6 ,5.2 })) #Set
+print(type((9.8 , 4.6 ,5.2))) #Tuple

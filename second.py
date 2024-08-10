41834170b40c2eb365a6a6482816db572b468561
Yash Tambe <109242952+YashT096@users.noreply.github.com>
2024-08-11 00:11:07 +0530

    Update second.py

--- second.py
+++ second.py
@@ -2,3 +2,23 @@
 
 First_name = "Yash"
 Last_name = "Tambe"
+Country = "India"
+City = "Sawantwadi"
+Age = "21"
+Skills = [HTML, CSS , Python ,JS ]
+
+#to Print 
+Personal Information = {
+  'firstname' : 'Yash' 
+   'Lastname' : ' Tambe'
+  'Country' : 'India'
+}
+# To print the Stored values
+
+print('First Name: ' , first_name )
+print('Last Name: ' , Last_name )
+print('Country:' , Country ) 
+print('City:' , City)
+print('Age: ', Age)
+print('Skills:', Skills)
+      

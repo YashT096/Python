98ebbfcc2f5d3506f0c5b8475b07ba7bb0663e82
Yash Tambe <109242952+YashT096@users.noreply.github.com>
2024-08-11 22:09:18 +0530

    Create day3.py

---
+++ day3.py
@@ -0,0 +1,47 @@
+#Arthimatic Operations in Python 
+#Integers
+
+print('Addittionn:', 2 + 4)
+print('Subtraction:', 5 - 2)
+print('Multiplication:', 3 * 2)
+print('Division:' , 10/2)
+print('Module:', 3 % 2)
+
+#Flotating Numbers 
+print('floating numbers, PI , 3.14)
+print('Floating numbers , gravity' ,9.81)
+
+#Complex number      
+print('complex Number' , 1+ 1j)
+
+#Declaring the variable at the tops first
+
+a = 5 # LHS is var and RHS is int
+b = 3 # LHS is var and RHS is int
+
+#Arithmatic Operations and Assigning results 
+total = a + b
+diff = a - b 
+product = a * b
+division = a / b
+remainder = a % b
+floor_division = a // b
+exponential = a ** b 
+
+
+print('a + b = ', total)
+print('a - b = ', diff)
+print('a * b = ', product)
+print('a / b = ', division)
+print('a % b = ', remainder)
+print('a // b = ', floor_division)
+print('a ** b = ', exponential)
+
+#Area of circle 
+radius = 10 
+area_of_circle = 3.14 * radius**2
+print("Area of Circle:" , area_of_circle )
+
+
+
+      

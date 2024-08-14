65fe5b63819271f46bd47feb9b5d1898a9b378dc
Yash Tambe <109242952+YashT096@users.noreply.github.com>
2024-08-14 22:22:39 +0530

    Create day5.py

---
+++ day5.py
@@ -0,0 +1,30 @@
+empty_list - list() #this is an empty list , no item in the list
+print(len(empty_list)) #0
+
+fruits = [ ' banana' , ' orange' , ' Mango' , ' lemon ']
+animal_products = ['milk' , 'Meat', 'Butter' , 'eggs']
+Countries = [ 'India', "Japan' , 'Denmark" ]
+
+# Print the lists and it length
+print('Fruits:', fruits)
+print('Number of fruits:', len(fruits))
+print('Vegetables:', vegetables)
+print('Number of vegetables:', len(vegetables))
+print('Animal products:',animal_products)
+print('Number of animal products:', len(animal_products))
+print('Web technologies:', web_techs)
+print('Number of web technologies:', len(web_techs))
+print('Number of countries:', len(countries))
+
+# Modifying list
+
+fruits = ['banana', 'orange', 'mango', 'lemon'] 
+first_fruit = fruits[0] # we are accessing the first item using its index
+print(first_fruit)      # banana
+second_fruit = fruits[1]
+print(second_fruit)     # orange
+last_fruit = fruits[3]
+print(last_fruit) # lemon
+# Last index
+last_index = len(fruits) - 1
+last_fruit = fruits[last_index]

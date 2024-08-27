8b233fdfa2309ee968a3b03693acf154c47f14e2
Yash Tambe <109242952+YashT096@users.noreply.github.com>
2024-08-28 00:12:59 +0530

    Update Day16.py

--- Day16.py
+++ Day16.py
@@ -1,1 +1,86 @@
 #Hello hello
+#Today we are going to know about Date and time in python
+
+#Python has got datetime module to handle date and time
+
+import datetime
+print(dir(datetime))
+['MAXYEAR', 'MINYEAR', '__builtins__', '__cached__', '__doc__', '__file__', '__loader__', '__name__', '__package__', '__spec__', 'date', 'datetime', 'datetime_CAPI', 'sys', 'time', 'timedelta', 'timezone', 'tzinfo']
+
+#Getting Date and time Info...
+
+from datetime import datetime
+now = datetime.now()
+print(now)                      # 2024-27-08 12:02:46.549883
+day = now.day                   # 27
+month = now.month               # 8
+year = now.year                 # 2024
+hour = now.hour                 # 12
+minute = now.minute             # 02
+second = now.second
+timestamp = now.timestamp()
+print(day, month, year, hour, minute)
+print('timestamp', timestamp)
+print(f'{day}/{month}/{year}, {hour}:{minute}')  # 27/08/2024, 12:02
+
+#Formatting Date Output using Strftime
+
+from datetime import datetime
+new_year = datetime(2020, 1, 1)
+print(new_year)      # 2020-01-01 00:00:00
+day = new_year.day
+month = new_year.month
+year = new_year.year
+hour = new_year.hour
+minute = new_year.minute
+second = new_year.second
+print(day, month, year, hour, minute) #1 1 2020 0 0
+print(f'{day}/{month}/{year}, {hour}:{minute}')  # 1/1/2020, 0:0
+
+#String to time in python
+
+from datetime import datetime
+date_string = "27 August, 2024"
+print("date_string =", date_string)
+date_object = datetime.strptime(date_string, "%d %B, %Y")
+print("date_object =", date_object)
+
+
+#Time objects to represent time 
+
+from datetime import time
+# time(hour = 0, minute = 0, second = 0)
+a = time()
+print("a =", a)
+# time(hour, minute and second)
+b = time(10, 30, 50)
+print("b =", b)
+# time(hour, minute and second)
+c = time(hour=10, minute=30, second=50)
+print("c =", c)
+# time(hour, minute, second, microsecond)
+d = time(10, 30, 50, 200555)
+print("d =", d)
+
+#Difference Between Two Points in Time Using
+
+today = date(year=2019, month=12, day=5)
+new_year = date(year=2020, month=1, day=1)
+time_left_for_newyear = new_year - today
+# Time left for new year:  27 days, 0:00:00
+print('Time left for new year: ', time_left_for_newyear)
+
+t1 = datetime(year = 2019, month = 12, day = 5, hour = 0, minute = 59, second = 0)
+t2 = datetime(year = 2020, month = 1, day = 1, hour = 0, minute = 0, second = 0)
+diff = t2 - t1
+print('Time left for new year:', diff) # Time left for new year: 26 days, 23: 01: 00
+
+#Difference Between Two Points in Time Using timedelata
+
+from datetime import timedelta
+t1 = timedelta(weeks=12, days=10, hours=4, seconds=20)
+t2 = timedelta(days=7, hours=5, minutes=3, seconds=30)
+t3 = t1 - t2
+print("t3 =", t3)
+
+

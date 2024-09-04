bb8da9a9a119813c7b069cb96b595d3ac0cef2ad
Yash Tambe <109242952+YashT096@users.noreply.github.com>
2024-09-04 11:39:03 +0530

    Update Day22.py

--- Day22.py
+++ Day22.py
@@ -1,1 +1,24 @@
 #hello Hellooo
+#Today we are going to learn web scraping
+#intially install pip requests on terminal , then install the beautifulsoup4 
+import requests
+from bs4 import BeautifulSoup
+
+req = requests.get("https://www.geeksforgeeks.org/")
+
+soup= BeautifulSoup(req.content, "html.parser")
+
+print(soup.prettify())
+ #to Run the following code you have to type : python "Your FIle name "
+ #as in my case i wrote :python day22.py on terminal 
+
+#to Get text directly
+
+import requests
+from bs4 import BeautifulSoup
+
+req = requests.get("https://www.geeksforgeeks.org/")
+
+soup= BeautifulSoup(req.content, "html.parser")
+
+print(soup.get_text())

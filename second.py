9da3ca756409f30a847fc21e3427270d3f25606b
Yash Tambe <109242952+YashT096@users.noreply.github.com>
2025-09-22 02:05:39 +0530

    Update second.py

--- second.py
+++ second.py
@@ -3,22 +3,22 @@
 first_name = "Yash"
 last_name = "Tambe"
 Country = "India"
-City = "sawantwadi"
+City = "Sawantwadi"
 Age = "21"
 Skills = ['HTML','CSS' , 'Python' ,'JS' ]
 
 #to Print 
 Personal_Information = {
   'firstname' : 'Yash' 
-   'lastname' : ' Tambe'
-  'Country' : 'India'
+  'lastname'  : 'Tambe'
+  'Country'   : 'India'
 }
 # To print the Stored values
 
 print('First Name: ' , first_name )
-print('Last Name: ' , last_name )
-print('Country:' , Country ) 
-print('City:' , City)
-print('Age: ', Age)
-print('Skills:', Skills)
+print('Last Name:  ' , last_name )
+print('Country:    ' , Country ) 
+print('City:       ' , City)
+print('Age:        ', Age)
+print('Skills:     ', Skills)
       
